47fe71afff7f1c48ed7163ac23b8840846dc7843
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2024-12-02 18:26:15 +0530

    fixidIssue

--- crud-rout/src/components/Read.jsx
+++ crud-rout/src/components/Read.jsx
@@ -3,22 +3,25 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Read = () => {
-  const [userSingleData, setSingleData] = useState();
+  const [userSingleData, setSingleData] = useState({});
   const { userId } = useParams();
 
   useEffect(() => {
-    axios.get(`http://localhost:3000/userData/`).then((response) => {
-      const filterData = response.data.filter((data) => data.id == userId);
-      setSingleData(filterData);
+    axios.get(`http://localhost:3000/userData/${userId}`).then((response) => {
+      setSingleData(response.data);
+      console.log(response.data); // to see the fetched data in console.log
     });
   }, []);
 
-  return <div>
-
-  </div>;
+  return (
+    <div>
+      <h1>User Details</h1>
+      <p>Name: {userSingleData.name}</p>
+      <p>Email: {userSingleData.email}</p>
+    </div>
+  );
 };
 
 export default Read;
-
 
 // create read file with your own
